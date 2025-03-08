47e657bd38b147ed7cfa759ab0eafa24c63427ca
manish <baghelmanish660@gmail.com>
2025-03-08 14:46:31 +0900

    initial idea

--- src/dubit.ts
+++ src/dubit.ts
@@ -582,6 +582,12 @@ export function routeTrackToDevice(
     context: audioContext,
     sourceNode: sourceNode,
     pullElement: pullElement,
+    pause: function () {
+      this.pullElement.pause();
+    },
+    resume: function () {
+      this.pullElement.play();
+    },
     stop: function () {
       this.sourceNode.disconnect();
       this.pullElement.pause();
